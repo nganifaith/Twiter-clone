9749ddf0d2c6123154ff265a16630283d3c71ca6
ngani faith <nganifaith@gmail.com>
2021-11-29 23:37:15 +0100

    Add content styles

--- src/components/SocailSignUp.styles.js
+++ src/components/SocailSignUp.styles.js
@@ -3,6 +3,9 @@ import styled  from 'styled-components';
 
 export const Wrapper = styled.div`
     background-color: black;
+    align-content: flex-start;
+    padding: 10px 0 5px 10px;
+    color: white;
 `;
 
 export const Content = styled.div`
@@ -11,3 +14,27 @@ export const Content = styled.div`
   justify-content: center;
 
 `;
+
+export const TextContent = styled.div`
+  padding: 5px;
+  margin: 10px;
+  line-height: 1.5px;
+  font-weight: bolder;
+`;
+
+export const SignUp = styled.div`
+  margin: 10px;
+  border-radius: 5px;
+  padding: 15px;
+  display: flex;
+  align-content: center;
+  justify-content: center;
+
+  icon {
+    width: 25%;
+  };
+
+  h3 {
+    flex: 1;
+  }
+`
